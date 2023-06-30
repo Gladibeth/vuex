8dcc2271cd257f00d22d75d08008ab886aa41bdd
Gladibeth <gladibethdelosa@gmail.com>
2023-06-30 16:00:00 -0500

    mutations

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,6 +2,14 @@
   <div>
     <h1>Accedo directo: {{  $store.state.counter  }}</h1>
     <h2>Counter computed {{ counterComputed }}</h2>
+
+
+    <button @click="increment">1+</button>
+    <button @click="incrementBy">5+</button>
+    <button @click="incrementRandom">Random</button>
+
+
+    
     <h1>mapState array</h1>
     <h2>{{ counter }}</h2>
     <h1>mapState obj</h1>
@@ -22,6 +30,18 @@ import { mapState } from 'vuex'
         count: state => state.counter,
         lastMutation: state => state.lastMutation
       })
+    },
+    methods:{
+      increment(){
+        this.$store.commit('increment');
+      },
+      incrementBy(){
+        this.$store.commit('incrementBy', 5);
+      },
+      incrementRandom(){
+        const random = Math.random() * 100;
+        this.$store.commit('incrementRandom', random.toFixed(0));
+      }
     }
   }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,20 @@ export default createStore({
     state: {
       counter: 1,
       lastMutation: 'none'
+    },
+    // mutaciones son SINCRONAS
+    mutations:{
+      increment( state ){
+        state.counter++;
+        state.lastMutation = 'increment';
+      },
+      incrementBy( state, val ){
+        state.counter += val;
+        state.lastMutation = 'incrementBy';
+      },
+      incrementRandom( state, val ){
+        state.counter += val;
+        state.lastMutation = 'incrementRandom';
+      }
     }
 })
